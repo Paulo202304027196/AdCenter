08bbfa3f88469ed64dabdb83848100d52db112a0
SkellingtonDEV <131309122+Paulo202304027196@users.noreply.github.com>
2024-04-08 10:56:21 -0300

    Update routes.py

--- AdCenter2/app/routes.py
+++ AdCenter2/app/routes.py
@@ -16,26 +16,8 @@ def init_app(app):
     
     @app.route("/")
     def inicio():        
-        return render_template("/inicio.html", usuarios=db.session.execute(db.select(usuario).order_by(usuario.id)).scalars())
-
-    @app.route("/conta")
-    def conta():        
-        return render_template("/conta01.html", contil=db.session.execute(db.select(contabilidade).order_by(contabilidade.id)).scalars())
-    
-    @app.route("/excluir/<int:id>")
-    def excluir_user(id):
-        delete=usuario.query.filter_by(id=id).first()
-        db.session.delete(delete)
-        db.session.commit()
-        return redirect(url_for("inicio"))
-    
-    @app.route("/excluir_conta/<int:id>")
-    def excluir_conta(id):
-        delete=contabilidade.query.filter_by(id=id).first()
-        db.session.delete(delete)
-        db.session.commit()
-        return redirect(url_for("conta"))
-    
+        return render_template("/inicio.html")
+            
     @app.route("/cad_user")
     def cad_user():        
         return render_template("cad_user.html")
