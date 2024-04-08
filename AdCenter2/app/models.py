cda9e3256ba804515845a37ef71c51481067cf94
SkellingtonDEV <131309122+Paulo202304027196@users.noreply.github.com>
2024-04-08 10:57:09 -0300

    Update models.py

--- AdCenter2/app/models.py
+++ AdCenter2/app/models.py
@@ -13,19 +13,17 @@ class usuario(db.Model, UserMixin):
     senha = db.Column(db.String(255), nullable=False)
     data = db.Column(db.DateTime,  default=db.func.current_timestamp(), onupdate=db.func.current_timestamp()) 
 
-class produto(db.Model):
-    __tablename__ = "produto"
-    id = db.Column(db.Integer, primary_key=True)    
-    nome = db.Column(db.String(255), nullable=True)
-    tipo = db.Column(db.String(255), nullable=True)
-    quantidade = db.Column(db.Integer, nullable=True)
-    data = db.Column(db.DateTime,  default=db.func.current_timestamp(), onupdate=db.func.current_timestamp()) 
+#class produto(db.Model):
+    #__tablename__ = "produto"
+    #id = db.Column(db.Integer, primary_key=True)    
+    #nome = db.Column(db.String(255), nullable=True)
+    #tipo = db.Column(db.String(255), nullable=True)
+    #quantidade = db.Column(db.Integer, nullable=True)
+    #data = db.Column(db.DateTime,  default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
     
-class contabilidade(db.Model):
-    __tablename__ = "contabilidade"
-    id = db.Column(db.Integer, primary_key=True)    
-    fatura = db.Column(db.String(255), nullable=True)
-    fiscal = db.Column(db.String(255), nullable=True)
-    data = db.Column(db.DateTime,  default=db.func.current_timestamp(), onupdate=db.func.current_timestamp()) 
-    
-   
+#class contabilidade(db.Model):
+    #__tablename__ = "contabilidade"
+    #id = db.Column(db.Integer, primary_key=True)    
+    #fatura = db.Column(db.String(255), nullable=True)
+    #fiscal = db.Column(db.String(255), nullable=True)
+    #data = db.Column(db.DateTime,  default=db.func.current_timestamp(), onupdate=db.func.current_timestamp()) 
